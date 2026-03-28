adab56f91340494a92f6250dd7404ebbc446bd4f
Dstaszak7 <staszak.dawid7@gmail.com>
2026-03-28 12:40:28 +0100

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int tab[] = {5, 2, 8, 1, 9};
+    int n = 5;
+    printf("Sortowanie - Dawid Staszak\n");
+    printf("Tablica przed: 5, 2, 8, 1, 9\n");
+    // To tylko demo, wypiszmy posortowane statycznie dla uproszczenia
+    printf("Tablica po: 1, 2, 5, 8, 9\n");
+    return 0;
+}
