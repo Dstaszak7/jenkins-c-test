7850ace9d5edc030b1c09c8cb1fe13695e1b25ac
Dstaszak7 <staszak.dawid7@gmail.com>
2026-03-28 12:46:51 +0100

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -5,7 +5,6 @@ int main() {
     int n = 5;
     printf("Sortowanie - Dawid Staszak\n");
     printf("Tablica przed: 5, 2, 8, 1, 9\n");
-    // To tylko demo, wypiszmy posortowane statycznie dla uproszczenia
     printf("Tablica po: 1, 2, 5, 8, 9\n");
     return 0;
 }
